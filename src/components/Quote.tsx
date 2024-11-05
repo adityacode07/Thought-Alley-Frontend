911825cc2ca97f2183ac0bc6ce39bd4d6baac901
Aditya Yadav <adi.yadav1715@gmail.com>
2024-11-06 03:40:09 +0530

    landingpagefixed

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,16 +1,18 @@
+import man from '../assets/outpu.jpg';
+
 function Quote() {
   return (
-    <div className="bg-slate-200 h-screen flex flex-col justify-center  px-14">
-      <div className="text-3xl font-bold">
-      "Welcome to the Blogging Web App.
-       Please Signup or Signin to continue and Enjoy"
-       </div>
-       <div className="text-lg font-medium pt-5 pl-1">
-        Aditya Yadav
-       </div>
-       <div className="text-base font-medium text-gray-700">
-        -The developer of this App.
-       </div>
+    <div
+      className="h-screen flex flex-col justify-center items-center px-14"
+      style={{ backgroundColor: '#ffffff' }}
+    >
+      <div className="text-3xl font-bold pt-4 ">
+        <img
+          src={man} 
+          alt="Art"
+          style={{ width: '108.5%', maxWidth: 'none' }} 
+        />
+      </div>
     </div>
   );
 }
